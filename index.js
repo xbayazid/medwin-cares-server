0ba02e3c60502bf22fb98d4516318f7e4b604625
xbayazid <bayazid9006@gmail.com>
2023-10-30 20:59:23 +0600

    limited  one booking per day

--- index.js
+++ index.js
@@ -44,14 +44,78 @@ async function run(){
     const bookingsCollection = client.db('medwinCares').collection('bookings');
 
     app.get('/appointmentOptions', async(req, res) =>{
+      const date = req.query.date;
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
-      res.send(options)
-    })
+      //get the booking of provided date
+      const bookingQuery = {appointmentDate: date}
+      const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+      options.forEach(option =>{
+        const optionBooked = alreadyBooked.filter(book =>  book.treatment === option.name);
+        const bookedSlots = optionBooked.map(book => book.slot);
+        const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot));
+        option.slots = remainingSlots;
+      })
+      res.send(options);
+    });
+
+    app.get('/v2/appointmentOptions', async (req, res) => {
+      const date = req.query.date;
+      const options = await appointmentOptionCollection.aggregate([
+          {
+              $lookup: {
+                  from: 'bookings',
+                  localField: 'name',
+                  foreignField: 'treatment',
+                  pipeline: [
+                      {
+                          $match: {
+                              $expr: {
+                                  $eq: ['$appointmentDate', date]
+                              }
+                          }
+                      }
+                  ],
+                  as: 'booked'
+              }
+          },
+          {
+              $project: {
+                  name: 1,
+                  slots: 1,
+                  booked: {
+                      $map: {
+                          input: '$booked',
+                          as: 'book',
+                          in: '$$book.slot'
+                      }
+                  }
+              }
+          },
+          {
+              $project: {
+                  name: 1,
+                  slots: {
+                      $setDifference: ['$slots', '$booked']
+                  }
+              }
+          }
+      ]).toArray();
+      res.send(options);
+  })
 
     app.post('/bookings', async(req, res) =>{
       const booking = req.body
       console.log(booking);
+      const query = {
+        appointmentDate : booking.appointmentDate
+      }
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
+      if (alreadyBooked.length){
+        const message = `You already have an booking on ${booking.appointmentDate}`
+        return res.send({acknowledged: false, message});
+      }
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     })
