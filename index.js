46c2871f769eefdd5344c55e8638d2d59eb55fd5
xbayazid <bayazid9006@gmail.com>
2023-10-31 17:36:18 +0600

    jwt token installed & implemented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const app = express();
@@ -37,6 +38,25 @@ async function run() {
 }
 run().catch(console.dir);
 // MongoDB connection ended 
+
+function verifyJWT(req, res, next) {
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send('unauthorized access');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+  })
+
+}
 
 async function run(){
   try{
@@ -106,12 +126,18 @@ async function run(){
       res.send(options);
   })
 
-  app.get('/bookings', async(req, res) =>{
+  app.get('/bookings', verifyJWT, async (req, res) => {
     const email = req.query.email;
-    const query = {email: email};
+    const decodedEmail = req.decoded.email;
+
+    if (email !== decodedEmail) {
+        return res.status(403).send({ message: 'forbidden access' });
+    }
+
+    const query = { email: email };
     const bookings = await bookingsCollection.find(query).toArray();
     res.send(bookings);
-  });
+})
 
     app.post('/bookings', async(req, res) =>{
       const booking = req.body
@@ -129,12 +155,23 @@ async function run(){
       const result = await bookingsCollection.insertOne(booking);
       res.send(result);
     });
+
+    app.get('/jwt', async(req, res) =>{
+      const email = req.query.email;
+      const query = {email:email};
+      const user = await usersCollection.findOne(query);
+      if(user){
+        const token  = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+        return res.send({accessToken: token});
+      }
+      res.status(403).asend({accessToken: ''})
+    })
     
     app.post('/users', async(req, res) =>{
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       res.send(result);
-    })
+    });
   }
   finally{
 
