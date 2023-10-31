e4d05df1c75abaad08e1fc45a218df1522e15ac0
xbayazid <bayazid9006@gmail.com>
2023-10-31 11:53:20 +0600

    used post method to save user data

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ async function run(){
   try{
     const appointmentOptionCollection = client.db('medwinCares').collection('appointmentOptions');
     const bookingsCollection = client.db('medwinCares').collection('bookings');
+    const usersCollection = client.db('medwinCares').collection('users');
 
     app.get('/appointmentOptions', async(req, res) =>{
       const date = req.query.date;
@@ -105,11 +106,20 @@ async function run(){
       res.send(options);
   })
 
+  app.get('/bookings', async(req, res) =>{
+    const email = req.query.email;
+    const query = {email: email};
+    const bookings = await bookingsCollection.find(query).toArray();
+    res.send(bookings);
+  });
+
     app.post('/bookings', async(req, res) =>{
       const booking = req.body
       console.log(booking);
       const query = {
-        appointmentDate : booking.appointmentDate
+        appointmentDate : booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment
       }
       const alreadyBooked = await bookingsCollection.find(query).toArray();
       if (alreadyBooked.length){
@@ -117,6 +127,12 @@ async function run(){
         return res.send({acknowledged: false, message});
       }
       const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+    
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     })
   }
