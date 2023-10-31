a03a2c59f22272d3f88974aab9f1d89f7e9a4d96
xbayazid <bayazid9006@gmail.com>
2023-10-31 17:52:41 +0600

    created api for all users data

--- index.js
+++ index.js
@@ -166,6 +166,12 @@ async function run(){
       }
       res.status(403).asend({accessToken: ''})
     })
+
+    app.get('/users', async(req, res)=>{
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    })
     
     app.post('/users', async(req, res) =>{
       const user = req.body;
